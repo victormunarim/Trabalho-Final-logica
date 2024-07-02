3f811fc7456b0d3fead7e036266391e03c8b6004
victormunarim <117938295+victormunarim@users.noreply.github.com>
2024-07-01 22:59:50 -0300

    Adição da parte do Bruno

---
+++ scriptFilmes.js
@@ -0,0 +1,47 @@
+let filmes = []
+
+function enviarFormFilme(evento){
+    evento.preventDefault()
+    const formulario = document.querySelector('#filminho')
+    const dadosForm = new FormData(formulario)
+
+    const url = dadosForm.get('url')
+    const titulo = dadosForm.get('nomeFilme')
+    const anoLancamento = dadosForm.get('anoLançamento')
+    const genero = dadosForm.get('genero')
+    const duracao = dadosForm.get('duracao')
+    const descricao = dadosForm.get('descricao')
+
+    let filme = []
+
+    filme.push(url,titulo,anoLancamento,genero,duracao,descricao)
+    filmes.push(filme)
+
+    localStorage.setItem('filmes',JSON.stringify(filmes))
+
+}
+
+let main = document.querySelector('#listaFilme')
+const filmesLocalStorage = localStorage.getItem('filmes')
+if(filmesLocalStorage){
+    
+    filmes = JSON.parse(filmesLocalStorage)
+    for(let i=0;i<filmes.length;i++){
+        
+main.innerHTML +=
+        `<div class="campo-filmes">
+            <img src="${filmes[i][0]}" alt="Foto do ${filmes[i][3]}">
+            <p>Título: ${filmes[i][1]}</p>
+            <p>Ano de Lançamento: ${filmes[i][2]}</p>
+            <p>Gênero: ${filmes[i][3]}</p>
+            <p>Duração: ${filmes[i][4]}</p>
+            <p>Sinopse: ${filmes[i][5]}</p>
+            <button class="botao-excluir" onclick="excluirLista${i}()">Excluir</button>
+        </div>`
+    }
+}
+
+main.innerHTML +=
+`<div class="campo-cadastrar">
+            <a class="botao-adicionar" href="cadastro-filme.html">+</a>
+</div>`
